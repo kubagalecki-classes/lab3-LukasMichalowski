cb39c8b890e3adb93df0fd7104b7328509f328be
LukasMichalowski <lukpio97@gmail.com>
2020-11-14 16:19:36 +0100

    Test 0

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -5,12 +5,16 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     if((towar = 0)) { return 0;}
+    int zaglowiec=0;
     for(int i = 0; i<towar; i++)
     {
         Stocznia stocznia{};
         Statek* s = stocznia();
         i = i + s->transportuj();
+        if(Zaglowiec* x= dynamic_cast<Zaglowiec>(s)){
+            zaglowiec+=1;
+        }
         delete s;
     }
-    return 0;
+    return zaglowiec;
 }
