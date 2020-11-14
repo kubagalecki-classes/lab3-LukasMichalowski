07649b4a2252342e08b69b3858f5b3ee69816d85
LukasMichalowski <lukpio97@gmail.com>
2020-11-14 18:10:26 +0100

    try fix to pass tests

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,17 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    if((towar = 0)) { return 0;}
-    unsigned int zaglowiec=0;
+    if((towar = 0))
+    {
+        return 0;
+    }
     Stocznia stocznia{};
-    int t=0;
-   while(t<towar)
+    unsigned int zaglowiec=0;
+    unsigned int t=0;
+
+    while(t<towar)
     {
         Statek* s = stocznia();
         t = t + s->transportuj();
         Zaglowiec* x= dynamic_cast<Zaglowiec*>(s);
-        if(x != nullptr ){
-            zaglowiec+=1;
+        if(x != nullptr){
+            zaglowiec++;
         }
         delete s;
     }
