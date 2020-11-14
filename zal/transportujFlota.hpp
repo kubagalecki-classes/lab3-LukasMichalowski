06014bfaef2981ae4290aa2209e5277b6ed46aa1
LukasMichalowski <lukpio97@gmail.com>
2020-11-14 17:54:29 +0100

    try fix

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,13 +6,13 @@ unsigned int transportujFlota(unsigned int towar)
 {
     if((towar = 0)) { return 0;}
     unsigned int zaglowiec=0;
-    Stocznia stocznia();
+    Stocznia stocznia{};
     int t=0;
    while(t<towar)
     {
         Statek* s = stocznia();
         t = t + s->transportuj();
-        Zaglowiec* x= dynamic_cast<Zaglowiec*>(s)
+        Zaglowiec* x= dynamic_cast<Zaglowiec*>(s);
         if(x != null ){
             zaglowiec+=1;
         }
